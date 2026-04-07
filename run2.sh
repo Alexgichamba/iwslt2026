46ceba84d3068932a2c5df8f39901695c5157cbd
Alexgichamba <alexgichamba@ieee.org>
2026-04-07 10:53:34 -0400

    Stop tracking run scripts

--- run2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source /ocean/projects/cis250145p/gichamba/miniconda3/etc/profile.d/conda.sh
-conda activate wakanda_stt
-echo "Active environment: $CONDA_DEFAULT_ENV"
-export HF_HOME=/ocean/projects/cis250145p/gichamba/huggingface
-./scripts/train_st.sh configs/ablations/p1d_transformer.yaml
-./scripts/train_st.sh configs/ablations/p1e_qformer.yaml
